95a83818af69735d29198d7111e3662a3d0201c9
Joe Grotto <joegrotto@heir.company>
2018-03-25 07:11:28 -0500

    ci keys

--- config/ci.js
+++ config/ci.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://127.0.0.1:27017/advancednode_ci',
-  redisURL: 'redis://127.0.0.1:6379'
+  redisURL: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAJA444F64C7H7VL5A',
+  secretAccessKey: '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'
 };
